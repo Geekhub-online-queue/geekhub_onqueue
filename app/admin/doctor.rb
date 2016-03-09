c4391bfe11c596ce42493a55f76ef3c71f0c8103
AndriyPedan <andriypedan.p@gmail.com>
2016-03-09 14:46:56 +0200

    Add photos and thumbnails to doctors

--- app/admin/doctor.rb
+++ app/admin/doctor.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Doctor do
-  permit_params :name, :phone, :specialization, :photo, :description, :hospital_id
+  permit_params :photo, :name, :phone, :specialization, :description, :hospital_id, :photo_file_name, :photo_content_type, :photo_file_size, :photo_updated_at
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+   f.inputs "Details" do
+    f.input :name
+    f.input :phone
+    f.input :specialization
+    f.input :description
+    f.input :hospital_id, :as => :select, :collection => Hospital.all.map {|u| [u.title.to_s, u.id]}
+    f.input :photo, :as => :file, :hint => f.template.image_tag(f.object.photo.url(:medium))
+  end
+  f.actions
+end
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305221521) do
+ActiveRecord::Schema.define(version: 20160309080328) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -50,11 +50,14 @@ ActiveRecord::Schema.define(version: 20160305221521) do
     t.string   "name"
     t.string   "phone"
     t.string   "specialization"
-    t.string   "photo"
     t.string   "description"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "hospital_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
