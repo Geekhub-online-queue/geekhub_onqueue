f714875385df72dd9d8d6ec86a0d037bd006db1c
AndriyPedan <andriypedan.p@gmail.com>
2016-03-16 13:17:36 +0200

    Bugfix

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,7 @@
 class Record < ActiveRecord::Base
   belongs_to :doctor
-  
-  validates :data, uniqueness: { scope: :doctor_id }, presence: true # TODO. fix this validation
+
+  validates :data, uniqueness: { scope: :doctor }, presence: true # TODO. fix this validation
   validates :name, presence: true
   validates :phone, presence: true
 end
