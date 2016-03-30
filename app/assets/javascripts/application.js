183a269d4636898938ec76fa802d8cdcc699f72b
AndriyPedan <andriypedan.p@gmail.com>
2016-03-30 22:56:36 +0300

    Add preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,18 @@
 //= require gmaps/google
 //= require turbolinks
 //= require_tree .
+$(function() {
+  $('#pictureInput').on('change', function(event) {
+    var files = event.target.files;
+    var image = files[0]
+    var reader = new FileReader();
+    reader.onload = function(file) {
+      var img = new Image();
+      console.log(file);
+      img.src = file.target.result;
+      $('#target').html(img);
+    }
+    reader.readAsDataURL(image);
+    console.log(files);
+  });
+});
