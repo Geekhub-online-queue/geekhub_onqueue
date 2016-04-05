f01b7f8465bd495ebad9820aacc30ef3e399a90b
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-04-06 00:17:47 +0300

    fix hospital photo

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -6,7 +6,13 @@ class Hospital < ActiveRecord::Base
   after_validation :geocode
   has_many :doctors
 
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo,
+    :default_url => ActionController::Base.helpers.asset_path('hospitals/missing.png'),
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :dropbox_options => {:path => proc { |style| "hospitals/#{id}/#{style}/#{photo.original_filename}" }}
+
+  #has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
   def normalize_friendly_id(text)
