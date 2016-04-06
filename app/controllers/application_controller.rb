1f6cfd5d64d10f8ee185f63f6013e190ca2d9ccd
YaroslavTupitskyi <jaroslav.typitsky@gmail.com>
2016-04-06 18:15:14 +0300

    add phone number to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :photo, :date_of_birth, :number_card, :email, :password) }
+     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :photo, :date_of_birth, :phone, :number_card, :email, :password) }
      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :date_of_birth, :number_card, :password, :current_password, :photo) }
+     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :date_of_birth, :phone, :number_card, :password, :current_password, :photo) }
   end
 end
